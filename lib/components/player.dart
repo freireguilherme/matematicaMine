10d7b5c18d9ac553ef5782a654eccf3ba5a131e8
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2022-08-19 01:35:36 -0300

    add: component player, a garra. apenas o aquivo

---
+++ lib/components/player.dart
@@ -0,0 +1,8 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+
+class Player extends SpriteAnimationComponent
+    with HasGameRef, CollisionCallbacks {
+  //TODO player class (player will be the hook)
+}
