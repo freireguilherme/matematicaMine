f105d61216debe174086cef9c3ff1830df29bc4f
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2022-08-19 01:33:04 -0300

    add: MainGamePage

---
+++ lib/main_game_page.dart
@@ -0,0 +1,25 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'mina.dart';
+
+class MainGamePage extends StatefulWidget {
+  const MainGamePage({Key? key}) : super(key: key);
+
+  @override
+  MainGameState createState() => MainGameState();
+}
+
+class MainGameState extends State<MainGamePage> {
+  Mina game = Mina();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
+        body: Stack(
+          children: [
+            GameWidget(game: game),
+          ],
+        ));
+  }
+}
