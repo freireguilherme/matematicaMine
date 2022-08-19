db10da1a28ccb2ca097b4e95c923f874724fd699
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2022-08-19 10:08:54 -0300

    TODO: collisions

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -4,5 +4,26 @@ import 'package:flame/sprite.dart';
 
 class Player extends SpriteAnimationComponent
     with HasGameRef, CollisionCallbacks {
+  @override
+  Future<void> onLoad() async {
+    add(RectangleHitbox());
+  }
   //TODO player class (player will be the hook)
+  /*@override
+  void onCollision(Set<Vector2> points, PositionComponent other) {
+    if (other is ScreenHitbox) {
+      //...
+    } else if (other is YourOtherComponent) {
+      //...
+    }
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent other) {
+    if (other is ScreenHitbox) {
+      //...
+    } else if (other is YourOtherComponent) {
+      //...
+    }
+  }*/
 }
