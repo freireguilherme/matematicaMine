c4535c54fe6a09f7ceace9d9927151554b49a89d
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2022-08-18 23:09:07 -0300

    add assets/images/

--- lib/mina.dart
+++ lib/mina.dart
@@ -4,6 +4,6 @@ import 'package:flame/game.dart';
 class Mina extends FlameGame {
   @override
   Future<void> onLoad() async {
-    await Flame.images.load('mina-sprites.png');
+    await Flame.images.load('bg_level_C.png');
   }
 }
